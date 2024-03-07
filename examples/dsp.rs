e3bb56bec375e52955e3130bb63c9c095d770eac
Tommie Nygren <tommie.nygren@gmail.com>
2024-03-07 13:12:59 +0100

    Working example using fun_dsp, yay!

--- examples/dsp.rs
+++ examples/dsp.rs
@@ -7,18 +7,22 @@ use rusty_music::musicians::bassist::Bassist;
 use rusty_music::musicians::conductor::Conductor;
 use rusty_music::musicians::drummer::Drummer;
 use rusty_music::musicians::soloist::Soloist;
+use rusty_music::MusicPlugin;
 use rusty_music::player::Intensity;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(AudioPlugin)
+        .add_plugins(MusicPlugin {
+            beats: 4,
+            note_type: 4,
+            bpm: 120.0,
+        })
         .add_plugins(DspPlugin::default())
         .add_systems(Update, change_intensity)
-        .add_systems(Startup, setup)
         .add_dsp_source(sine_wave, SourceType::Static { duration: 0.5 })
         .add_dsp_source(triangle_wave, SourceType::Static { duration: 0.5 })
-        .add_systems(Update, interactive_audio)
+        .add_systems(Startup, setup)
         .run();
 }
 
@@ -79,70 +83,84 @@ fn interactive_audio(
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    mut assets: ResMut<Assets<AudioSource>>,
+    dsp_manager: Res<DspManager>,
 ) {
+    let source = dsp_manager
+        .get_graph(triangle_wave)
+        .unwrap_or_else(|| panic!("DSP source not found!"));
+    let audio_source = DefaultBackend::convert_to_audio_source(source.clone());
+    let audio_source = assets.add(audio_source);
+
     commands.spawn(
         Musician::new(
             "Melody".to_string(),
             Sampler {
-                handle: asset_server.load("samples/lo-fi/construction/120/acid/short/c.wav"),
-                volume: 0.251188643150958,
+                handle: audio_source,
+                volume: 1.0,
             },
             Soloist::new("Solo".to_string(), 4, 4, 2),
             MusicianType::Solo,
         ));
+
+    let source = dsp_manager
+        .get_graph(triangle_wave)
+        .unwrap_or_else(|| panic!("DSP source not found!"));
+    let audio_source = DefaultBackend::convert_to_audio_source(source.clone());
+    let audio_source = assets.add(audio_source);
+
     commands.spawn(
         Musician::new(
             "Bassist".to_string(),
             Sampler {
-                handle: asset_server.load("samples/lo-fi/construction/120/bass/c.wav"),
+                handle: audio_source,
                 volume: 0.7,
             },
             Bassist::new("Bass".to_string()),
             MusicianType::Bass,
         ));
 
-    commands.spawn(
-        Musician::new(
-            "Kick".to_string(),
-            Sampler {
-                handle: asset_server.load("samples/drums/kit-d/kick.wav"),
-                volume: 1.0,
-            },
-            Drummer {
-                name: "Kick".to_string(),
-                notes: generate_kick_beat(),
-            },
-            MusicianType::Drums,
-        )
-    );
-    commands.spawn(
-        Musician::new(
-            "Kick".to_string(),
-            Sampler {
-                handle: asset_server.load("samples/drums/kit-d/snare.wav"),
-                volume: 1.0,
-            },
-            Drummer {
-                name: "Kick".to_string(),
-                notes: generate_snare_beat(),
-            },
-            MusicianType::Drums,
-        )
-    );
-
-    commands.spawn(Musician::new(
-        "Hihat".to_string(),
-        Sampler {
-            handle: asset_server.load("samples/drums/kit-d/hihat.wav"),
-            volume: 1.0,
-        },
-        Drummer {
-            name: "HiHat".to_string(),
-            notes: generate_hihat_beat(),
-        },
-        MusicianType::Drums,
-    ));
+    // commands.spawn(
+    //     Musician::new(
+    //         "Kick".to_string(),
+    //         Sampler {
+    //             handle: asset_server.load("samples/drums/kit-d/kick.wav"),
+    //             volume: 1.0,
+    //         },
+    //         Drummer {
+    //             name: "Kick".to_string(),
+    //             notes: generate_kick_beat(),
+    //         },
+    //         MusicianType::Drums,
+    //     )
+    // );
+    // commands.spawn(
+    //     Musician::new(
+    //         "Kick".to_string(),
+    //         Sampler {
+    //             handle: asset_server.load("samples/drums/kit-d/snare.wav"),
+    //             volume: 1.0,
+    //         },
+    //         Drummer {
+    //             name: "Kick".to_string(),
+    //             notes: generate_snare_beat(),
+    //         },
+    //         MusicianType::Drums,
+    //     )
+    // );
+    //
+    // commands.spawn(Musician::new(
+    //     "Hihat".to_string(),
+    //     Sampler {
+    //         handle: asset_server.load("samples/drums/kit-d/hihat.wav"),
+    //         volume: 1.0,
+    //     },
+    //     Drummer {
+    //         name: "HiHat".to_string(),
+    //         notes: generate_hihat_beat(),
+    //     },
+    //     MusicianType::Drums,
+    // ));
 
     commands.insert_resource(Conductor {
         chords: generate_chords()
