d0f9835da5fa38b489c93fa5cc0277ebef7b8c29
Tommie Nygren <tommie.nygren@gmail.com>
2024-03-12 15:09:43 +0100

    Made some easy-to-use-methods available for the library.

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,12 +1,8 @@
 use bevy::DefaultPlugins;
 use bevy::prelude::{App, AssetServer, ButtonInput, Commands, KeyCode, Res, ResMut, Startup, Update};
-use rusty_music::musicians::{Musician, Sampler};
-use rusty_music::musicians::bassist::Bassist;
 use rusty_music::musicians::conductor::Conductor;
-use rusty_music::musicians::drummer::{Drummer, generate_hihat_beat, generate_kick_beat, generate_snare_beat};
-use rusty_music::musicians::soloist::Soloist;
+use rusty_music::musicians::drummer::{generate_hihat_beat, generate_kick_beat, generate_snare_beat};
 use rusty_music::{generate_chords, MusicPlugin};
-use rusty_music::musicians::arpegggiator::Arpeggiator;
 use rusty_music::player::Intensity;
 
 fn main() {
@@ -47,68 +43,31 @@ fn setup(
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn(
-        Musician::new(
-            "Melody".to_string(),
-            Soloist::new(Sampler {
-                handle: asset_server.load("samples/lo-fi/construction/120/acid/short/c.wav"),
-                volume: 0.251188643150958,
-            }, 2, 4, 2),
-        ));
+        rusty_music::create_soloist("Melody".to_string(),
+                                    asset_server.load("samples/lo-fi/construction/120/acid/short/c.wav"),
+                                    0.251188643150958,
+                                    2,
+                                    4,
+                                    2));
     commands.spawn(
-        Musician::new(
-            "Arpeggio".to_string(),
-            Arpeggiator::new(Sampler {
-                handle: asset_server.load("samples/lo-fi/construction/120/acid/long/c.wav"),
-                volume: 0.251188643150958,
-            }),
-        ));
-    commands.spawn(
-        Musician::new(
-            "Bassist".to_string(),
-            Bassist::new(Sampler {
-                handle: asset_server.load("samples/lo-fi/construction/120/bass/c.wav"),
-                volume: 0.7,
-            }),
-        ));
+        rusty_music::create_arpeggiator("Arpeggio".to_string(), asset_server.load("samples/lo-fi/construction/120/acid/long/c.wav"), 0.251188643150958));
 
     commands.spawn(
-        Musician::new(
-            "Kick".to_string(),
-            Drummer::new(
-                Sampler {
-                    handle: asset_server.load("samples/drums/kit-d/kick.wav"),
-                    volume: 1.0,
-                },
-                generate_kick_beat(),
-            ),
-        )
+        rusty_music::create_bassist("Bassist".to_string(), asset_server.load("samples/lo-fi/construction/120/bass/c.wav"), 0.7));
+
+    commands.spawn(
+        rusty_music::create_drummer("Kick".to_string(), asset_server.load("samples/drums/kit-d/kick.wav"), 1.0, generate_kick_beat())
     );
     commands.spawn(
-        Musician::new(
-            "Kick".to_string(),
-            Drummer::new(
-                Sampler {
-                    handle: asset_server.load("samples/drums/kit-d/snare.wav"),
-                    volume: 1.0,
-                },
-                generate_snare_beat(),
-            ),
-        )
+        rusty_music::create_drummer("Snare".to_string(), asset_server.load("samples/drums/kit-d/snare.wav"), 1.0, generate_snare_beat())
     );
 
-    commands.spawn(Musician::new(
-        "Hihat".to_string(),
-        Drummer::new(
-            Sampler {
-                handle: asset_server.load("samples/drums/kit-d/hihat.wav"),
-                volume: 1.0,
-            },
-            generate_hihat_beat(),
-        ),
-    ));
+
+    commands.spawn(
+        rusty_music::create_drummer("Hihat".to_string(), asset_server.load("samples/drums/kit-d/hihat.wav"), 1.0, generate_hihat_beat())
+        );
 
     commands.insert_resource(Conductor {
         chords: generate_chords()
     });
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use bevy::app::{App, Plugin, Update};
-use bevy_kira_audio::{AudioPlugin};
+use bevy_kira_audio::{AudioPlugin, AudioSource};
+use bevy::asset::Handle;
+use bevy::utils::HashMap;
 use crate::clock::{Beat, Clock, progress_clock_system};
-use crate::musicians::{Chord, Note};
+use crate::musicians::{Chord, Musician, Note, Sampler};
+use crate::musicians::arpegggiator::Arpeggiator;
+use crate::musicians::bassist::Bassist;
+use crate::musicians::drummer::Drummer;
+use crate::musicians::soloist::Soloist;
 use crate::player::{Intensity, play_sound_on_the_beat};
 
 pub mod clock;
@@ -79,4 +85,48 @@ pub fn generate_chords() -> Vec<Chord> {
             Note::new(3, 0.1),
         ], scale_notes.clone()),
     ]
+}
+
+pub fn create_drummer(name: String, handle: Handle<AudioSource>, volume: f64, drum_beat: HashMap<(u32, u32), Note>) -> Musician {
+    Musician::new(
+        name,
+        Drummer::new(
+            Sampler {
+                handle,
+                volume,
+            },
+            drum_beat,
+        ),
+    )
+}
+
+pub fn create_arpeggiator(name: String, handle: Handle<AudioSource>, volume: f64) -> Musician {
+    Musician::new(
+        name,
+        Arpeggiator::new(Sampler {
+            handle,
+            volume,
+        }),
+    )
+}
+
+pub fn create_soloist(name: String, handle: Handle<AudioSource>, volume: f64, record_bars: u32, beats_per_bar: u32, repeats: u32) -> Musician {
+    Musician::new(
+        name,
+        Soloist::new(Sampler {
+            handle,
+            volume,
+        }, record_bars, beats_per_bar, repeats),
+    )
+}
+
+
+pub fn create_bassist(name: String, handle: Handle<AudioSource>, volume: f64) -> Musician {
+    Musician::new(
+        name,
+        Bassist::new(Sampler {
+            handle,
+            volume,
+        }),
+    )
 }
